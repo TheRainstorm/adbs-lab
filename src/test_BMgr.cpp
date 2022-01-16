32150e9125d17fea10d486ee0d22acf6d9e5a226
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2022-01-16 13:48:46 +0800

    profile time

--- src/test_BMgr.cpp
+++ src/test_BMgr.cpp
@@ -1,6 +1,7 @@
 #include "BMgr.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
     const char *trace_file = "../data-5w-500000-zipf.txt";
@@ -14,27 +15,38 @@ int main(){
     int type, page_id;
     int cnt = 0;
 
+    long long t;
+    t = -clock();
     BMgr bmgr(0);
     fseek(fp, 0, SEEK_SET);
     while((fscanf(fp, "%d,%d", &type, &page_id))==2){
         bmgr.accessPage(page_id, type);
         // printf("cnt: %d\n", cnt++);
     }
+    t += clock();
     bmgr.print_statistical_data();
+    printf("CLOCKS_PER_SEC=%d\n", CLOCKS_PER_SEC);
+    printf("time elapsed=%lld, %f\n",t, (float)t/CLOCKS_PER_SEC);
 
+    t = -clock();
     BMgr bmgr2(1);
     fseek(fp, 0, SEEK_SET);
     while((fscanf(fp, "%d,%d", &type, &page_id))==2){
         bmgr2.accessPage(page_id, type);
     }
+    t += clock();
     bmgr2.print_statistical_data();
+    printf("time elapsed=%lld, %f\n",t, (float)t/CLOCKS_PER_SEC);
 
+    t = -clock();
     BMgr bmgr3(2);
     fseek(fp, 0, SEEK_SET);
     while((fscanf(fp, "%d,%d", &type, &page_id))==2){
         bmgr3.accessPage(page_id, type);
     }
+    t += clock();
     bmgr3.print_statistical_data();
+    printf("time elapsed=%lld, %f\n",t, (float)t/CLOCKS_PER_SEC);
     
     fclose(fp);
     return 0;
